cf47fcc2861ea84494042132d8d86fa0e4904e7f
Marco <iljrss98@gmail.com>
2026-04-12 16:59:34 -0300

    chore(cleanup): remove orphan components and dead hooks

--- src/components/organisms/Board3d/spaceElements/domain/ndcCoordinates.ts
+++
@@ -1,8 +0,0 @@
-// Zero imports. Testável com Node.js puro.
-
-export function screenToNDC(clientX, clientY, rect) {
-  return {
-    x: ((clientX - rect.left) / rect.width) * 2 - 1,
-    y: -((clientY - rect.top) / rect.height) * 2 + 1,
-  };
-}
